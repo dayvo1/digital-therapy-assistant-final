4d646934541c92d03d92e2d848c597e03da82c80
gc <chavezagreyson@gmail.com>
2026-05-13 20:41:57 -0700

    added eslint

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,22 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    // This part replaces the old "ignore" files
+    ignores: [".next/*", "out/*", "build/*"],
+  }
+];
+
+export default eslintConfig;
