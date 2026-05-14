9efe128df62a63df20551a4abcd6eeac40b15960
gc <chavezagreyson@gmail.com>
2026-05-13 21:58:09 -0700

    added terraform file

---
+++ deploy/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "key_pair_name" {
+  description = "The name of the SSH key pair to use for the EC2 instance"
+  type        = string
+  default     = "E90"
+}
+
+variable "anthropic_api_key" {
+  description = "The API key used by the digital therapy assistant backend"
+  type        = string
+  sensitive   = true # This hides the key from being printed in logs
+}
