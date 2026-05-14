6d2ea3633b6be8a264439f12859e0cfdbb00549e
gc <chavezagreyson@gmail.com>
2026-05-13 23:18:18 -0700

    added terraform file

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -24,7 +24,7 @@ data "aws_ami" "amazon_linux_2023" {
 }
 
 resource "aws_security_group" "app_sg" {
-  name        = "therapy-app-sg4"
+  name        = "therapy-app-sg5"
   description = "Allow SSH and App Traffic"
 
   ingress {
